f3209e4585ba5e68a2176da06be2dd0629dd144e
Wayne咏辰 <75261164+wayne931121@users.noreply.github.com>
2022-10-27 00:24:22 +0800

    Update url_decode.py

--- url_decode.py
+++ url_decode.py
@@ -23,3 +23,37 @@ def url_decoder(b):
         result.append(ord(i))
     result = result.decode(encoding="utf-8")
     return result
+
+#保留字元的百分號編碼
+URL_RFC_3986 = {
+"!": "%21", "#": "%23", "$": "%24", "&": "%26", "'": "%27", "(": "%28", ")": "%29", "*": "%2A", "+": "%2B", 
+",": "%2C", "/": "%2F", ":": "%3A", ";": "%3B", "=": "%3D", "?": "%3F", "@": "%40", "[": "%5B", "]": "%5D",
+}
+
+def url_encoder(b):
+    # https://zh.wikipedia.org/wiki/%E7%99%BE%E5%88%86%E5%8F%B7%E7%BC%96%E7%A0%81
+    if type(b)==bytes:
+        b = b.decode(encoding="utf-8") #byte can't insert utf8 charater
+    result = bytearray() #bytearray: rw, bytes: read-only
+    enter_hex_unicode_mode = 0
+    hex_tmp = ""
+    now_index = 0
+    for i in b:
+        if i in URL_RFC_3986:
+            for j in URL_RFC_3986[i]:
+                result.append(ord(j))
+            continue
+        i = bytes(i, encoding="utf-8")
+        if len(i)==1:
+            result.append(ord(i))
+        else:
+            k = 0
+            for c in i:
+                c = hex(c)[2:].upper()
+                result.append(ord("%"))
+                result.append(ord(c[0:1]))
+                result.append(ord(c[1:2]))
+    result = result.decode(encoding="ascii")
+    return result
+
+#print(url_encoder("我好棒==%%0.0:)")) ==> '%E6%88%91%E5%A5%BD%E6%A3%92%3D%3D%%0.0%3A%29'
